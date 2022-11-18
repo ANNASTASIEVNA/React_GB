2666f4d1779629425399641780b0146de0e65208
annanastya <anastasiia93156@gmail.com>
2022-11-18 16:18:52 +0300

    ls7
    ls7

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,28 @@
-import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import { configureStore, combineReducers,compose,applyMiddleware } from '@reduxjs/toolkit';
+import thunk from 'redux-thunk';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import { profileReducer } from './profile/reduser';
 import { chatsReduser } from './chats/reduser';
 import { messagesReduser } from './messages/reduser';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const persistConfig = {
+  key: 'GBChat',
+  storage,
+};
+
+const rooteRducer = combineReducers({
+  profile: profileReducer,
+  chatsList: chatsReduser,
+  messages: messagesReduser
+});
+
+const persistedReducer = persistReducer(persistConfig, rooteRducer);
+
 export const store = configureStore(
-  combineReducers({
-    profile: profileReducer,
-    chatsList: chatsReduser,
-    messages: messagesReduser
-  }),
-  window.__REDUX_DEVTOOLS_EXTENSION__ &&
-  window.__REDUX_DEVTOOLS_EXTENSION__());
+  persistedReducer,
+  composeEnhancers(applyMiddleware(thunk)));
+
+export const persistor = persistStore(store);
